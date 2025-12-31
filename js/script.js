1522daebba55be4c238732e7b8987b544c85629b
Sh3rrJ <sh3riffimpact@gmail.com>
2025-12-31 23:24:06 +0700

    end of lesson 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
 'use strict';
 
 const numberOfFilms = +prompt("Сколько фильмов вы уже смотрели?", '');
@@ -10,15 +24,29 @@ const personalMovieDB = {
   privat: false
 };
 
-const movie1 = prompt('Один из последних просмотренных фильмов?', ''),
-      rank1 = prompt('На сколько оцените его?', ''),
-      movie2 = prompt('Один из последних просмотренных фильмов?', ''),
-      rank2 = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+  const movie = prompt('Один из последних просмотренных фильмов?', ''),
+        rank = prompt('На сколько оцените его?', '');
+  if (movie != null && rank != null && movie != '' && rank != '' && movie.length < 50) {
+    personalMovieDB.movies[movie] = rank;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+};
 
-personalMovieDB.movies[movie1] = rank1;
-personalMovieDB.movies[movie2] = rank2;
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count <= 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка")
+};
 
-console.log(personalMovieDB)
+console.log(personalMovieDB);
 
 
 
